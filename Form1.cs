97575666c7adaca39dc7caf518e06738721537f5
Ismael Avila <ismael.guada234015@gmail.com>
2022-03-14 14:39:14 -0600

    Se agregaron atributos a la clase. Bitmas para el análisis de la imagen, la representación del grafo y para la animación. El agente y el objetivo. La lista de círculos y el propio grafo. También se inicializa la lista del círculos en el constructor.

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,21 @@ namespace SemAlgoritmia
 {
     public partial class Form1 : Form
     {
+        Bitmap bmpImage;
+        Bitmap bmpIDs;
+        Bitmap bmpAnimation;
+
+        Agent agent;
+        Objetive objetive;
+
+        List<Circle> circleList;
+        Graph graph;
+
         public Form1()
         {
             InitializeComponent();
+
+            circleList = new List<Circle>();
         }
     }
 }
