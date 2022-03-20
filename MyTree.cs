dfe3f3f1e2e35d9146530a66a29aa18ad7d7de31
Ismael Avila <ismael.guada234015@gmail.com>
2022-03-20 17:33:01 -0600

    Se hizo la impementación de un árbol n-ésimo para utilizarlo con DFS y BFS.

---
+++ MyTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemAlgoritmia
+{
+    internal class MyTree
+    {
+        MyTreeNode root;
+
+        public MyTree()
+        {
+            root = new MyTreeNode();
+        }
+
+        public MyTree(Vertex data)
+        {
+            root = new MyTreeNode(data, null);
+        }
+
+        public void insert(Vertex data, MyTreeNode node)
+        {
+            node.addChild(data);
+        }
+
+        public void inorder(MyTreeNode root)
+        {
+            Console.Write(root.Data.Id.ToString());
+
+            for(int i=0; i< root.ChildrenCount; i++) {
+                inorder(root.getChildAt(i));
+                Console.Write(root.Data.Id.ToString());
+            }
+        }
+
+    }
+}
---
+++ MyTreeNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemAlgoritmia
+{
+    internal class MyTreeNode
+    {
+        List<MyTreeNode> children;
+        Vertex data;
+        MyTreeNode father;
+
+        public MyTreeNode()
+        {
+            children = new List<MyTreeNode>();
+        }
+
+        public MyTreeNode(Vertex data, MyTreeNode father)
+        {
+            children = new List<MyTreeNode>();
+
+            this.data = data;
+            this.father = father;
+        }
+
+        public void addChild(Vertex data)
+        {
+            children.Add(new MyTreeNode(data, this));
+        }
+
+        public MyTreeNode getChildAt(int pos)
+        {
+            return children[pos];
+        }
+
+        public Vertex Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public int ChildrenCount
+        {
+            get { return children.Count; }
+        }
+
+    }
+}
