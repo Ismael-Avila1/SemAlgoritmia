a98283091989c5254e9a7df899a4573aa6c4fe09
Ismael Avila <ismael.guada234015@gmail.com>
2022-05-18 20:19:15 -0500

    Se creó la clase DijkstraElement que se usará para el algoritmo de Dijkstra

---
+++ DijkstraElement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemAlgoritmia
+{
+    internal class DijkstraElement
+    {
+        bool definitive;
+        float accumulatedWeight;
+        Vertex comimgFrom;
+
+
+        public DijkstraElement()
+        {
+
+        }
+
+        public DijkstraElement(bool definitive, float accumulatedWeight, Vertex comimgFrom) 
+        {
+            this.definitive = definitive;
+            this.accumulatedWeight = accumulatedWeight;
+            this.comimgFrom = comimgFrom;
+        }
+
+
+        public bool Definitive
+        {
+            get { return definitive; }
+            set { definitive = value; }
+        }
+
+        public float AccumulatedWeight
+        {
+            get { return accumulatedWeight; }
+            set { accumulatedWeight = value; }
+        }
+
+        public Vertex ComimgFrom
+        {
+            get { return comimgFrom; }
+            set { comimgFrom = value; }
+        }
+
+
+    }
+}
